0501b58a158eb821d56c4a7266893a9244db0c58
Hesoten <hesoten@torikara.org>
2026-08-20 21:51:03 +0900

    Limit color 0 sprite collisions in sprite mode 1 to the TMS99x8 family

--- blueMSX/Src/VideoChips/SpriteLine.h
+++ blueMSX/Src/VideoChips/SpriteLine.h
@@ -78,6 +78,7 @@ UInt8* spritesLine(VDP* vdp, int line) {
     int collision;
     int dispLine;
     int solidColor;
+    int color0Collides;
 
     idx = line;
 
@@ -101,6 +102,8 @@ UInt8* spritesLine(VDP* vdp, int line) {
     size   = vdpIsSprites16x16(vdp->vdpRegs) ? 16 : 8;
     scale  = vdpIsSpritesBig(vdp->vdpRegs) ? 2 : 1;
     solidColor = vdpIsColor0Solid(vdp->vdpRegs) ? 1 : 0;
+    /* A transparent color 0 sprite still collides on the TMS99x8 family, but not on the V99x8. */
+    color0Collides = vdpIsMsx1Vdp(vdp);
     dispLine = line;
     line   = (line + vdpVScroll(vdp)) & 0xff;
     
@@ -191,6 +194,10 @@ UInt8* spritesLine(VDP* vdp, int line) {
         patternPtr = &vdp->vram[(vdp->sprGenBase & (-1 << 11)) + ((int)(attrib[2] & patternMask) << 3) + spriteLine[visibleCnt]];
 
         if (!solidColor && rawColor == 0) {
+            if (!color0Collides) {
+                continue;
+            }
+
             if (scale == 1) {
                 pattern = patternPtr[0]; 
                 if (pattern) {
